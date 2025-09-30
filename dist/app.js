11168f5dea03b9d9870f99aedc9963de1d99bcb7
rhenzovat <rhenzovat@gmail.com>
2025-09-30 08:43:01 -0500

    librerias

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hola mundo");
-console.log("hola");
+const chalk_1 = __importDefault(require("chalk"));
+const figlet_1 = __importDefault(require("figlet"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+console.log(chalk_1.default.green('I am a green line ' +
+    chalk_1.default.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'));
+console.log(figlet_1.default.textSync("renzo"));
+console.log(chalk_1.default.green.bgRed.bold(figlet_1.default.textSync("renzo")));
+console.log(process.env.S3_BUCKET);
+console.log(process.env.MI_VARIABLE);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,19 @@
-console.log("hola mundo")
-console.log("hola")
+import chalk from 'chalk';
+import figlet from "figlet";
+import dotenv from 'dotenv'
+dotenv.config();
+
+console.log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
+
+console.log(figlet.textSync("renzo")
+);
+
+console.log(chalk.green.bgRed.bold(figlet.textSync("renzo"))
+);
+
+console.log(process.env.S3_BUCKET);
+console.log(process.env.MI_VARIABLE);
