d76dc9d828777a4382736c7feccb93562913d9a1
rhenzovat <rhenzovat@gmail.com>
2025-10-19 17:16:48 -0500

    feat: enhance app functionality with new routes and middleware chore: update environment variables and remove obsolete files

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,80 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express")); //importacion de la libreria
-require("dotenv/config");
-const app = (0, express_1.default)(); //Creacion de la aplicacion express
-const port = process.env.PORT;
-function miMiddleware(req, res, next) {
-    console.log('este es un middleware');
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-//creando un middleware
-app.use(miMiddleware);
-app.get('/usuario', (req, res) => {
-    console.log('este es mi servicio');
-    res.send('soy renzo PUERTO 3000'); //Enviar la respuesta al cliente
+import express, {} from 'express'; //importacion de la libreria
+import 'dotenv/config';
+// const app = express() //Creacion de la aplicacion express
+// const port = process.env.PORT||4000
+// function miMiddleware(req, res, next){
+// 	console.log('este es un middleware');
+// 	console.log(`${req.method} ${req.url}`)
+// 	next()
+// }
+// //creando un middleware
+// app.use(miMiddleware);
+// app.get('/usuario', (req, res) => { //definicion de un ruta y metodo HTTP
+// 	console.log('este es mi servicio')
+//   res.send('soy renzo PUERTO 3000') //Enviar la respuesta al cliente
+// })
+// app.listen(port, ()=>{
+// 	console.log(`El servidor esta corriendo en el puerto ${port}`)
+// })//poner el puerto en escucha
+const app = express(); //Creamos la aplicacion express
+const port = process.env.PORT || 4000;
+//midelword
+app.use(express.json());
+// (Medoto HTTP) + (base url+enpoit)
+app.get('/canchas', (req, resp) => {
+    // aca colocamos la logica
+    resp.json({ message: "esta es la respuesta del Listado" });
+});
+app.post('/canchas', (req, resp) => {
+    // const valores = req.body;
+    const { nombre, edad, direccion, isActive } = req.body;
+    const Persona = {
+        nombre: "jose",
+        edad: 22
+    };
+    const { nombre: nombrecito, edad: edades } = Persona;
+    console.log(nombre, edad, direccion, isActive);
+    console.log(nombrecito, edades);
+    console.log(nombre);
+    mostrarPersona(Persona);
+    // const Persona={
+    // 	name:"renzo",
+    // 	apellido:"vargas",
+    // 	edad: 25,
+    // 	vive:"manchay"
+    // }
+    // const {edad, name, vive, apellido} = Persona;
+    // console.log(vive, apellido,edad, name)
+    // console.log("se recibio: ",valores);
+    resp.status(200).json({
+        message: 'Creando una cancha',
+    });
+});
+app.get('/canchas/:id', (req, resp) => {
+    // aca colocamos la logica
+    const valores = req.params.id;
+    console.log(`esto son los valores: ${valores}`);
+    const usuario = {
+        id: 101,
+        nombre: "Renzo",
+        edad: 32,
+        pais: "peru",
+        ciudad: "lima",
+        activo: true
+    };
+    const { nombre, edad = 1, activo: isActive } = usuario;
+    console.log(nombre, edad, isActive);
+    function mostrarResumen({ nombre, edad, pais }) {
+        console.log(`mostrar: nombre ${nombre}, edad ${edad} y pais ${pais}`);
+    }
+    mostrarResumen(usuario);
+    resp.json({
+        message: "esta es la respuesta del Listado"
+    });
 });
 app.listen(port, () => {
-    console.log(`El servidor esta corriendo en el puerto ${port}`);
-}); //poner el puerto en escucha
-//# sourceMappingURL=app.js.map
+    console.log(`El servidor esta activo en el puerto ${port}`);
+});
+function mostrarPersona(persona) {
+    console.log(`Persona:${persona.nombre}, Edad: ${persona.edad}`);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,124 @@
-import express from 'express'//importacion de la libreria
+import express, { type Request, type Response } from 'express'//importacion de la libreria
 import 'dotenv/config'
-const app = express() //Creacion de la aplicacion express
-const port = process.env.PORT
 
-function miMiddleware(req, res, next){
-	console.log('este es un middleware');
-	console.log(`${req.method} ${req.url}`)
-	next()
-}
+// const app = express() //Creacion de la aplicacion express
+// const port = process.env.PORT||4000
 
-//creando un middleware
-app.use(miMiddleware);
+// function miMiddleware(req, res, next){
+// 	console.log('este es un middleware');
+// 	console.log(`${req.method} ${req.url}`)
+// 	next()
+// }
+
+// //creando un middleware
+// app.use(miMiddleware);
 
 
-app.get('/usuario', (req, res) => { //definicion de un ruta y metodo HTTP
-	console.log('este es mi servicio')
-  res.send('soy renzo PUERTO 3000') //Enviar la respuesta al cliente
+// app.get('/usuario', (req, res) => { //definicion de un ruta y metodo HTTP
+// 	console.log('este es mi servicio')
+//   res.send('soy renzo PUERTO 3000') //Enviar la respuesta al cliente
 
+// })
+
+// app.listen(port, ()=>{
+// 	console.log(`El servidor esta corriendo en el puerto ${port}`)
+// })//poner el puerto en escucha
+
+
+const app = express();//Creamos la aplicacion express
+const port = process.env.PORT || 4000;
+
+//midelword
+app.use(express.json());
+
+// (Medoto HTTP) + (base url+enpoit)
+app.get('/canchas', (req: Request, resp:Response)=>{
+// aca colocamos la logica
+resp.json({message:"esta es la respuesta del Listado"})
 })
 
-app.listen(port, ()=>{
-	console.log(`El servidor esta corriendo en el puerto ${port}`)
-})//poner el puerto en escucha
+
+app.post('/canchas', (req:Request, resp:Response)=>{
+	// const valores = req.body;
+
+	const{nombre, edad, direccion, isActive} = req.body; 
+	const Persona = {
+		nombre:"jose",
+		edad:22
+	}
+
+	const{nombre:nombrecito,edad:edades}= Persona
+	console.log(nombre, edad, direccion, isActive)
+	console.log(nombrecito, edades)
+	console.log(nombre)
+	mostrarPersona(Persona)
+	// const Persona={
+	// 	name:"renzo",
+	// 	apellido:"vargas",
+	// 	edad: 25,
+	// 	vive:"manchay"
+	// }
+	// const {edad, name, vive, apellido} = Persona;
+	
+	
+	// console.log(vive, apellido,edad, name)
 
 
+	// console.log("se recibio: ",valores);
+
+resp.status(200).json({
+	message: 'Creando una cancha',
+})
+})
+
+app.get('/canchas/:id', (req: Request, resp:Response)=>{
+// aca colocamos la logica
+
+const valores = req.params.id;
+console.log(`esto son los valores: ${valores}`)
+interface Usuario{
+	id:number;
+	nombre:String;
+	edad: number;
+	pais:String;
+	ciudad:String;
+	activo:boolean
+}
+const usuario = {
+	id:101,
+	nombre:"Renzo",
+	edad:32,
+	pais:"peru",
+	ciudad:"lima",
+	activo:true
+}
+
+const {nombre, edad=1, activo:isActive} = usuario;
+console.log(nombre, edad, isActive)
+
+function mostrarResumen({nombre,edad,pais}:Usuario ){
+ console.log(`mostrar: nombre ${nombre}, edad ${edad} y pais ${pais}`)
+ 
+}
+mostrarResumen(usuario)
+resp.json({
+	message:"esta es la respuesta del Listado"
+	})
+})
+
+
+
+
+
+app.listen(port, () =>{
+	console.log(`El servidor esta activo en el puerto ${port}`);
+})
+
+interface Persona{
+	nombre:string;
+	edad:number;
+
+}
+function mostrarPersona(persona:Persona){
+	console.log(`Persona:${persona.nombre}, Edad: ${persona.edad}`)
+}
